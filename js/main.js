02652974e938ce57e0797578978d52701375b488
Abner kallil <abner_kallil@hotmail.com>
2023-05-25 19:19:09 -0300

    Updated Search Engine

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const search_btn = document.querySelector("#search-btn")
 const url = "https://api.pokemontcg.io/v2/cards/"
+const pokeLogo = document.querySelector(".navbar-brand")
+pokeLogo.addEventListener("click", InitialPokefetch)
 search_btn.addEventListener("click", fetchPoke)
-let counter = 0
 
 async function fetchPoke(e){
-
+    let counter = 0
     e.preventDefault();
     const search_poke_name = document.querySelector("#search").value.toLowerCase()
     document.querySelector(".pokemon").innerHTML = ``
@@ -15,36 +16,52 @@ async function fetchPoke(e){
     .then((res) => res.json())
     .then((data) => {
         data.data.forEach(card => {
+            console.log(card)
             if (search_poke_name == card.name.toLowerCase()){
-                document.querySelector(".pokemon").innerHTML += `<img class="pokemon-card-searched" src=${card.images["small"]}>`
+                document.querySelector(".pokemon").innerHTML += `<a target="_blank" href="${card.tcgplayer.url}"><img  id=${card.id} class="pokemon-card-searched" src=${card.images["small"]}></a>`
+                
                 counter += 1          
             }
       
         })
         if (counter == 0){
+            document.querySelector("#searched-pokes").innerText = `Nothing Founded for "${search_poke_name}"`
             InitialPokefetch()
         }
         
     })
     .catch(error => {
-        // handle the error
+        console.log(`ERROR: ${error}`)
     });
 }
-async function InitialPokefetch(){
-    await fetch(url,{
+ function InitialPokefetch(){
+    document.querySelector(".pokemon").innerHTML = `` 
+    document.querySelector("#searched-pokes").innerText = `Loading Pokemons.. Please Wait!!`
+    let pokemonCount = 0
+    fetch(url,{
         method: "GET",
     })
     .then((res) => res.json())
     .then((data) => {
+         
         data.data.forEach(card => {
+            console.log(card)
+            pokemonCount += 1
             
-            document.querySelector(".pokemon").innerHTML += `<img class="pokemon-card" src=${card.images["small"]}>`  
+            document.querySelector(".pokemon").innerHTML += `<a target="_blank" href="${card.tcgplayer.url}"><img  id=${card.id} class="pokemon-card" src=${card.images["small"]}></a>` 
+            document.querySelector("#searched-pokes").innerText = `View All Pokemons: ${pokemonCount}`
         })
+        
+        
     })
     .catch(error => {
-        // handle the error
+        console.log(`ERROR: ${error}`)
     });
     
+}
+
+function showCardResume(){
+
 }
 InitialPokefetch()
 
